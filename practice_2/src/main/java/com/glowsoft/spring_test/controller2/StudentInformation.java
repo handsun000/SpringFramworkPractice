fce5e33829aaed0abe98d32880cbb5c8f3e3b44a
son jin yeong <handsun000@gmail.com>
2021-12-15 16:49:33 +0900

    StudentInformation

---
+++ practice_2/src/main/java/com/glowsoft/spring_test/controller2/StudentInformation.java
@@ -0,0 +1,13 @@
+package com.glowsoft.spring_test.controller2;
+
+import lombok.Data;
+
+@Data
+public class StudentInformation {
+
+    private String name;
+    private int age;
+    private int gradeNum;
+    private int classNum;
+
+}
