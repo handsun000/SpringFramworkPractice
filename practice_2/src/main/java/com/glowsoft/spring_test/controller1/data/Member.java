a03cf4b7f88a9861ba0ef1ab8a70ecb13d95a065
son jin yeong <handsun000@gmail.com>
2021-12-15 16:45:25 +0900

    Member

---
+++ practice_2/src/main/java/com/glowsoft/spring_test/controller1/data/Member.java
@@ -0,0 +1,12 @@
+package com.glowsoft.spring_test.controller1.data;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private String name;
+    private String id;
+    private String pw;
+    private String email;
+}
